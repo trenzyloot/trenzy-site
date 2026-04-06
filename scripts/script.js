b7300367a82d63dec2d8d6a98a0a3ed4531aa10a
Trenzy Loot <trenzyloot@gmail.com>
2026-04-07 01:42:27 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,103 @@
+let topProds = [
+    {
+        title: "Grey Polo T-Shirt",
+        desc: "Perfect for Summers🔥",
+        price: 449,
+        bestSeller: true,
+        img: "polos/greypolo.jpeg",
+        affLink: "https://amzn.to/4sOnARw",
+    },
+    {
+        title: "Red Polo T-Shirt",
+        desc: "Breathable + under budget",
+        price: 399,
+        bestSeller: false,
+        img: "polos/redpolo.jpeg",
+        affLink: "https://amzn.to/3O0m7sj",
+    },
+    {
+        title: "Black Polo T-Shirt",
+        desc: "Bughet + Style Combo",
+        price: 399,
+        bestSeller: true,
+        img: "polos/blackpolo.jpg",
+        affLink: "https://amzn.to/4cqxNhc",
+    },
+    
+    {
+        title: "White & Teal Polo T-Shirt",
+        desc: "Trending right now👀",
+        price: 299,
+        bestSeller: false,
+        img: "polos/stripepolo.jpeg",
+        affLink: "https://amzn.to/4dxr5Hg",
+    },
+    {
+        title: "Blue Polo T-Shirt",
+        desc: "Comfort + Breathable",
+        price: 498,
+        bestSeller: false,
+        img: "polos/bluepolo.jpeg",
+        affLink: "https://amzn.to/4bVzK5g",
+    },
+
+    {
+        title: "Lavender Polo T-Shirt",
+        desc: "Summers coolest fit🔥",
+        price: 379,
+        bestSeller: false,
+        img: "polos/lavenderpolo.png",
+        affLink: "https://amzn.to/41h3oM4",
+    },
+];
+
+
+prodTXT = ``;
+
+for (let i = 0; i < topProds.length; i++) {
+    console.log(i);
+    let prod = topProds[i];
+    
+    let isBestSeller = prod.bestSeller;
+    let bestSellerHTML = ``;
+    if (isBestSeller) {
+        bestSellerHTML =
+        `
+        <img src="images/bestSeller.png" alt="">
+        `;
+    }
+    else {
+        bestSellerHTML = ``;
+    }
+    
+    prodHTML =
+    `
+    <li class="card" id="card1">
+                        <div class="price-tag">${bestSellerHTML}</div>
+                        <div class="card-img-holder"><img src="images/prod/${prod.img}" alt=""></div>
+                        <div class="card-content-holder">
+                            <div class="product-details">
+                                <h1 class="product-name">${prod.title}</h1>
+                                <p class="product-desc">${prod.desc}</p>
+                            </div>
+                            <div class="cta">
+                                <div class="price">
+                                    ₹${prod.price}
+                                </div>
+                                <a href="${prod.affLink}" class="button">View On Amazon</a>
+                            </div>
+                        </div>
+                    </li>    
+    `
+    prodTXT += prodHTML;
+
+    
+    
+}
+
+document.querySelector(".main-card-holder").innerHTML = prodTXT
+
+
+
+
+
